c40c28fb9d2ea519d0376ab8063ef2c63bebebfb
Ganesh Suthar <67453173+ganeshappentus@users.noreply.github.com>
2023-02-28 18:04:24 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,59 @@
+import "regenerator-runtime/runtime";
 import React, { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
+import reactLogo from "./assets/LOGO.png";
+import { DebounceInput } from "react-debounce-input";
+
 import "./App.css";
 import axios from "axios";
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
+import Speech from "react-speech";
 
 function App() {
   const [text, setText] = useState("");
+  const {
+    transcript,
+    listening,
+    resetTranscript,
+    browserSupportsSpeechRecognition,
+  } = useSpeechRecognition();
+
+  if (!browserSupportsSpeechRecognition) {
+    alert("Browser doesn't support speech recognition.");
+  }
+
+  useEffect(() => {
+    setText(transcript);
+  }, [transcript]);
 
   return (
     <div className="App">
       <div>
-        <input
+        <img src={reactLogo} />
+      </div>
+      <div>
+        <h3>
+          This is a Translation Demo App developed by Appentus Technologies Pvt.
+          Ltd. for Parliament of India.
+        </h3>
+      </div>
+
+      <div>
+        <DebounceInput
+          minLength={2}
+          debounceTimeout={500}
           placeholder="Type here..."
-          style={{ width: 500, height: "auto" }}
-          type={"text"}
-          onChange={(e) => {
-            setText(e.target.value);
-          }}
+          onChange={(e) => setText(e.target.value)}
         />
+      </div>
+
+      <div>
+        <p>Microphone: {listening ? "on" : "off"}</p>
+        <button onClick={SpeechRecognition.startListening}>Start</button>
+        <button onClick={SpeechRecognition.stopListening}>Stop</button>
+        <button onClick={resetTranscript}>Reset</button>
+        <p>{transcript}</p>
       </div>
 
       <div>
@@ -71,29 +108,40 @@ const Convert = ({ text, language }) => {
   const [convertedText, setConvertedText] = useState("");
 
   useEffect(() => {
-    setConvertedText("AI is loading.")
-    const response = axios
-      .post(
-        "https://translation.googleapis.com/language/translate/v2",
-        {},
-        {
-          params: {
-            q: text,
-            target: language,
-            key: "AIzaSyASWiTxATD_-wH5M6ZsUrUHveTl6Mq4Sxo",
-          },
-        }
-      )
-      .then((response) => {
-        setConvertedText(response.data.data.translations[0].translatedText);
-      })
-      .catch((err) => {
-        setConvertedText("Translation error.")
-        console.log("Translation error.");
-      });
+    if (text != "") {
+      setConvertedText("AI is loading.");
+      const response = axios
+        .post(
+          "https://translation.googleapis.com/language/translate/v2",
+          {},
+          {
+            params: {
+              q: text,
+              target: language,
+              key: "AIzaSyASWiTxATD_-wH5M6ZsUrUHveTl6Mq4Sxo",
+            },
+          }
+        )
+        .then((response) => {
+          setConvertedText(response.data.data.translations[0].translatedText);
+        })
+        .catch((err) => {
+          setConvertedText("Translation error.");
+          console.log("Translation error.");
+        });
+    }
   }, [text, language]);
 
-  return <div>{convertedText}</div>;
+  return (
+    <div>
+      {convertedText}
+      <Speech
+        displayText={"play"}
+        text={convertedText}
+        voice="Google UK English Female"
+      />
+    </div>
+  );
 };
 
 export default App;
